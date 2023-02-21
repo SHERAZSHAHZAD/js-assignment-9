afd80c66364399aae6927c47f85c20cc79308bda
SHERAZSHAHZAD <119057763+SHERAZSHAHZAD@users.noreply.github.com>
2023-02-21 06:48:56 -0800

    First Commit

---
+++ scritp.js
@@ -0,0 +1,86 @@
+// // task first
+// let myVar= (5);
+// console.log(myVar); 
+
+// // task two
+// let mystring = ("Hello,world!");
+// alert(mystring);
+
+// // task three
+// let num1,num2,userNum;
+// num1 = parseInt(prompt("Enter any number"));
+// num2 = parseInt(prompt("Enter any number"));
+// userNum = (num1 + num2);
+// console.log(userNum);
+
+// // task four
+// let myBoolean = ture;
+// if(!myBoolean){
+// console.log("It's ture")
+// }
+// else{
+// console.log("It's false")
+// }
+
+// // task five
+// let a;
+// a = prompt("Enter any number");
+// if(a = even){
+//     console.log("Number is even");
+// }
+// else{
+//     console.log("Number is odd");
+// }
+
+// // task six
+// let b,c,d;
+// b = "hello";
+// c = "world";
+// d = (b + c);
+// console.log(d);
+
+// // task seven
+// let e,f,g;
+// e = "hello";
+// f = "world";
+// g = (b * c);
+// console.log(g);
+
+// // task nine
+
+// let number1,number2,number3;
+// number1 = prompt("Enter any number1");
+// number2 = prompt("Enter any number1");
+// number3 = prompt("Enter any number1");
+
+// if(number1 > number2 && number1 > number3) {  
+// Document.write("number1 is greater then number2 and number3")}
+// else if (number2 > number1 && number2 > number3)
+// {document.write("number1 is smaller then number2 and number3")}
+// else {
+// document.write("number1 is smaller then number2 and number3")
+// }
+
+// task eight
+
+let pass = prompt("Enter password");
+let pass1 = prompt("Enter password")
+if(pass == pass1)
+{
+    alert("access granted")
+}
+else{
+    alert("access denied")
+}
+
+// ten task
+
+let string = prompt("Enter password");
+let string1 = prompt("Enter password")
+if(string == string1)
+{
+    alert("The string are equal")
+}
+else{
+    alert("The striing are not equal")
+}
